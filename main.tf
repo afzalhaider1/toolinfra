20593a783d25fe5ea0c623cddcc702260833fba7
afzalhaider1 <131112928+afzalhaider1@users.noreply.github.com>
2023-09-15 11:55:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-    source = "github.com/afzalhaider1/toolinfra//module/vpc"
+    source = "github.com/afzalhaider1/toolinfra//module/vpc?ref=module"
 }
 module "ec2" {
-    source = "github.com/afzalhaider1/toolinfra//module/ec2"
+    source = "github.com/afzalhaider1/toolinfra//module/ec2?ref=module"
 }
